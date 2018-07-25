42cf4a3d6898fc48d747ab7e40a524d80061361f
sami-alajrami <sami@praqma.com>
2018-07-25 19:22:14 +0200

    updating the entrypoint script for the terraform-gcloud docker image

--- .circleci/docker-entrypoint.sh
+++ .circleci/docker-entrypoint.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
-# populating the json credentials file from env variable
-if [ ! -z "${GOOGLE_CREDENTIALS}" ]; then
-  echo $GOOGLE_CREDENTIALS > /tmp/credentials.json
-  if [ $? != 0 ]; then
-      echo "FAILED to write Google credentials into /tmp/credentials.json. Aborting!"
-      exit 1
-  else
-    echo "Successfully populated /tmp/credentials.json"
-    gcloud auth activate-service-account --key-file /tmp/credentials.json
-    if [ $? != 0 ]; then
-        echo "FAILED to authenticate to Google cloud. Aborting!"
-        exit 1
-    else
-      echo "Successfully authenticated to Google cloud."
-    fi
-  fi
+
+if [ "$CLUSTER" = "dev" ] && [ ! -z "${PI_DEV_GOOGLE_CREDENTIALS}" ]; then
+  echo $PI_DEV_GOOGLE_CREDENTIALS > /tmp/credentials.json
+elif [ "$CLUSTER" = "prod" ] && [ ! -z "${PI_PROD_GOOGLE_CREDENTIALS}" ]; then  
+  echo $PI_PROD_GOOGLE_CREDENTIALS > /tmp/credentials.json
 else
-  echo "Google credentials have not been set in the environment. Aborting!"
+  echo "Appropriate Google credentials have not been set in the environment. Aborting!"
   exit 1
 fi
 
+if [ $? != 0 ]; then
+  echo "FAILED to write Google credentials into /tmp/credentials.json. Aborting!"
+  exit 1
+else
+  echo "Successfully populated /tmp/credentials.json"
+  gcloud auth activate-service-account --key-file /tmp/credentials.json
+  if [ $? != 0 ]; then
+      echo "FAILED to authenticate to Google cloud. Aborting!"
+      exit 1
+  else
+    echo "Successfully authenticated to Google cloud."
+  fi
+fi
 
 exec "$@"
