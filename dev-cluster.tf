0bf6705b2549549f35d8fea9ff73aacfb02e1c40
Bjørn Remseth <rmz@comoyo.com>
2018-08-28 12:42:12 +0200

    Bumping version of cluster to 1.10.6-gke.2

--- dev-cluster.tf
+++ dev-cluster.tf
@@ -28,7 +28,7 @@ module "gke" {
   cluster_password    = "${var.cluster_admin_password}"
   cluster_name        = "pi-dev"
   cluster_description = "Development cluster for Ostelco Pi."
-  cluster_version     = "1.10.5-gke.4"
+  cluster_version     = "1.10.6-gke.2"
   cluster_zone        = "${var.cluster_zone}"
 
   # the line below makes the cluster multizone (regional)
